34665eb516fa0b12c0823e92f4582c0f8216e6ef
kwonha <s2118@e-mirim.hs.kr>
2022-05-19 18:39:21 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/s2118/mirim_project_0519_1/MainActivity.java
@@ -0,0 +1,14 @@
+fpackage kr.hs.emirim.s2118.mirim_project_0519_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
